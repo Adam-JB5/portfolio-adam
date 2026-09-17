b23f8402c495068073ef4d36625d06ceed1449bd
Adam-JB5 <adamjanah562005@gmail.com>
2026-09-18 00:00:48 +0200

    Deployment issues fixed. Swiper CSS files were not being loaded by initial bundle, needed to load them in the global scss file

--- src/app/sections/skills/skills.ts
+++ src/app/sections/skills/skills.ts
@@ -2,10 +2,6 @@ import { Component, signal, AfterViewInit } from '@angular/core';
 import { skillsData } from '../../core/data/skills.data';
 import Swiper from 'swiper';
 import { Navigation, Pagination } from 'swiper/modules';
-// import Swiper and modules styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
 
 @Component({
   imports: [],
